6d4a9b85f8df69ed1c090ae763b2e4c71e25009d
Hwangtaehun <101085674+Hwangtaehun@users.noreply.github.com>
2025-07-31 20:23:33 +0900

    수정

--- FileIO/main.c
+++ FileIO/main.c
@@ -9,9 +9,9 @@ void FileTest4();
 
 int main()
 {
-    //FileTest1();
-    //FileTest2();
-    //FileTest3();
+    FileTest1();
+    FileTest2();
+    FileTest3();
     FileTest4();
     return 0;
 }
@@ -31,32 +31,27 @@ void FileTest2()
     fprintf(outf, "fopen으로 출력: %d\n", n);
 }
 
-void FileTest3()
-{
-    FILE *inf = stdin;
-    FILE *outf = stdout;
+void FileTest3() {
+    FILE* inf = stdin;
+    FILE* outf = stdout;
     //FILE *inf = fopen("in.txt", "r");
     //FILE *outf = fopen("out.txt", "w");
     printf("정수 입력 2: ");
     fscanf(inf, "%d", &n);
-    fprintf(outf, "표준 출력 2: %d\n", n);
+    fprintf(outf, "표준 출력 2 : %d\n", n);
 }
 
-void FileTest4()
-{
+void FileTest4() {
     freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    //FILE *fi = freopen("in.txt", "r", stdin);
-    //FILE *fo = freopen("out3.txt", "w", stdout);
-
+    freopen("out3.txt", "w", stdout);
     scanf("%d", &n);
-    printf("freopen으로 재 설정 후 출력: %d\n", n);
+    printf("freopen으로 재 설정 후 출력 : %d\n", n);
     fclose(stdin);
     fclose(stdout);
     freopen("con", "r", stdin);
-    freopen("con", "w", stdin);
+    freopen("con", "w", stdout);
 
-    printf("표준입출력으로 다시 설정 후 입력: ");
+    printf("표준입출력으로 다시 설정 후 입력 : ");
     scanf("%d", &n);
-    printf("표준입출력으로 다시 설정 후 출력: %d\n", n);
+    printf("표준입출력으로 다시 설정 후 출력 : %d\n", n);
 }
