d0637061fa6f89c090e020b5d302676d0b7e27a7
Hwangtaehun <101085674+Hwangtaehun@users.noreply.github.com>
2025-07-31 20:30:35 +0900

    파일테스트

--- FileIO/main.c
+++ FileIO/main.c
@@ -9,33 +9,31 @@ void FileTest4();
 
 int main()
 {
-    FileTest1();
-    FileTest2();
-    FileTest3();
+    //FileTest1();
+    //FileTest2();
+    //FileTest3();
     FileTest4();
     return 0;
 }
 
-void FileTest1()
-{
-    printf("정수 입력: ");
+void FileTest1() {
+    printf("정수 입력 : ");
     scanf("%d", &n);
-    printf("표준 출력: %d\n", n);
+    printf("표준 출력 : %d\n", n);
 }
 
-void FileTest2()
-{
-    FILE *inf = fopen("in.txt", "r");
-    FILE *outf = fopen("out2.txt", "w");
+void FileTest2() {
+    FILE* inf = fopen("in.txt", "r");
+    FILE* outf = fopen("out2.txt", "w");
     fscanf(inf, "%d", &n);
-    fprintf(outf, "fopen으로 출력: %d\n", n);
+    fprintf(outf, "fopen으로 출력 : %d\n", n);
 }
 
 void FileTest3() {
-    FILE* inf = stdin;
-    FILE* outf = stdout;
-    //FILE *inf = fopen("in.txt", "r");
-    //FILE *outf = fopen("out.txt", "w");
+    FILE *inf = stdin;
+    FILE *outf = stdout;
+    //FILE* inf = fopen("in.txt", "r");
+    //FILE* outf = fopen("out.txt", "w");
     printf("정수 입력 2: ");
     fscanf(inf, "%d", &n);
     fprintf(outf, "표준 출력 2 : %d\n", n);
