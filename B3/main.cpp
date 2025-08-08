db2fb3496c4b5173ed3ab355245d7e9ed6209349
Hwangtaehun <r283@naver.com>
2025-08-08 22:06:37 +0900

    답+수정

--- B3/main.cpp
+++ B3/main.cpp
@@ -6,6 +6,47 @@ using namespace std;
 int g_size, g_cnt;
 bool **g_check;
 
+// 정답지 풀이
+int n, cnt;
+bool colChk[10], incChk[20], decChk[20];
+
+void TestPrn(int no, int row, int col){
+    printf("%d,%d,%d %d : ", no, row, col, cnt);
+
+    for(int i = 1; i <= n; i++){
+        printf("%d ", colChk[i] );
+    }
+    printf(" : ");
+    for(int i = 1; i <= n * 2; i++){
+        printf("%d ", incChk[i] );
+    }
+    printf(" : ");
+    for(int i = 1; i <= n * 2; i++){
+        printf("%d ", decChk[i] );
+    }
+    printf("\n");
+}
+
+void Dfs(int row){
+    if( row > n){
+        cnt++;
+        return;
+    }
+
+    for(int col = 1; col <= n; col++)
+    {
+        if( !colChk[col] && !incChk[row + col] && !decChk[n + (row - col)] ){
+            colChk[col] = incChk[row + col] = decChk[n + (row - col)] = true;
+            TestPrn(1, row, col);
+
+            Dfs(row + 1);
+
+            colChk[col] = incChk[row + col] = decChk[n + ( row - col )] = false;
+            TestPrn(2, row, col);
+        }
+    }
+}
+
 void input(){
     freopen("input.txt", "r", stdin);
     scanf("%d", &g_size);
@@ -46,10 +87,13 @@ void Print(int **data){
     }
     printf("\n");
 }
+
 void Solve(int **data, int x, int y){
     for(int i = 0; i < g_size; i++){
-        int m_x = x + i;
-        int m_y = y + i;
+        int m_x1 = x + i;
+        int m_y1 = y + i;
+        int m_x2 = x - i;
+        int m_y2 = y - i;
 
         if(data[x][i] == 0){
             data[x][i] = 2;
@@ -59,12 +103,29 @@ void Solve(int **data, int x, int y){
             data[i][y] = 2;
         }
 
-        if(m_x < g_size && m_y < g_size){
-            if(data[m_x][m_y] == 0){
-                data[m_x][m_y] = 2;
+        if(m_x1 < g_size && m_y1 < g_size){
+            if(data[m_x1][m_y1] == 0){
+                data[m_x1][m_y1] = 2;
             }
         }
 
+        if(m_x1 < g_size && m_y2 > -1 ){
+            if(data[m_x1][m_y2] == 0){
+                data[m_x1][m_y2] = 2;
+            }
+        }
+
+        if(m_x2 > -1 && m_y1 < g_size){
+            if(data[m_x2][m_y1] == 0){
+                data[m_x2][m_y1] = 2;
+            }
+        }
+
+        if(m_x2 > -1 && m_y2 > -1){
+            if(data[m_x2][m_y2] == 0){
+                data[m_x2][m_y2] = 2;
+            }
+        }
     }
 
     if(!Check(data)){
@@ -125,6 +186,10 @@ int main()
     delete[] data;
 
     output();
+
+    scanf("%d", &n);
+    Dfs(1);
+    printf("%d", cnt);
 
     return 0;
 }
