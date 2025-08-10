38c369e5647ab70b900e32fa95fee3aa46a64df7
Hwangtaehun <r283@naver.com>
2025-08-10 21:39:22 +0900

    B4 풀이완료
    B4 풀이완료

--- B4/main.cpp
+++ B4/main.cpp
@@ -7,9 +7,13 @@ int g_row, g_col, g_cnt = 0x7FFFFFFF;
 int g_dx[4] = {1, 0, -1, 0}, g_dy[4] = {0, 1, 0, -1};
 char **g_data;
 bool **g_visit;
+int visited[101][101];
+
+//explanation
+int sRow, sCol, gRow, gCol;
 
 struct VERTEX {
-    int x, y;
+    int x, y; // row, col
 };
 
 void Input(){
@@ -27,6 +31,10 @@ void Input(){
     for(int i = 0; i < g_row; i++){
         for(int j = 0; j < g_col; j++){
             scanf(" %c", &g_data[i][j]);
+            if(g_data[i][j] == 'S')
+                sRow=i, sCol=j;
+            else if(g_data[i][j] == 'G')
+                gRow=i, gCol=j, g_data[i][j]='.';
         }
     }
     fclose(stdin);
@@ -38,8 +46,52 @@ void Output(){
     fclose(stdout);
 }
 
-bool isSafe(int col, int row){
+bool isSafe(int row, int col){
     return (col >= 0 && col < g_col) && (row >= 0 && row < g_row);
+}
+
+void TestPrn(queue <VERTEX> q1, VERTEX cur)
+{
+    printf("%2d,%d Ã³¸® ÈÄ queue : ", cur, cur.x, cur.y);
+    while(!q1.empty()) {
+        printf("%d,%d ", q1.front().x, q1.front().y);
+        q1.pop();
+    }
+    printf("\n");
+    for(int i = 0; i < g_row; i++){
+        for(int j = 0; j < g_col; j++){
+            printf("%2d", visited[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int Solve()
+{
+    queue <VERTEX> q;
+    q.push((VERTEX){sRow, sCol});
+    visited[sRow][sCol] = 0;
+
+    while(!q.empty()){
+
+        VERTEX cur = q.front();
+        q.pop();
+
+        if(cur.x == gRow && cur.y == gCol)
+            break;
+
+        for(int i = 0; i < 4; i++){
+            int row = cur.x + g_dx[i], col = cur.y + g_dy[i];
+
+            if(isSafe(row, col) && !visited[row][col] && g_data[row][col] == '.') {
+                visited[row][col] = visited[cur.x][cur.y] + 1;
+                q.push((VERTEX){row, col});
+            }
+        }
+        TestPrn(q, cur);
+    }
+    return visited[gRow][gCol];
 }
 
 void Bfs(int r, int c) {
@@ -72,7 +124,7 @@ void Bfs(int r, int c) {
     }
 }
 
-void Solve() {
+void MySolve() {
     for(int i = 0; i < g_row; i++){
         for(int j = 0; j < g_col; j++){
             if(g_data[i][j] == 'S'){
@@ -95,8 +147,10 @@ void Print(){
 int main()
 {
     Input();
-    Solve();
-    Output();
+    //MySolve();
+    //Output();
+
+    printf("%d\n", Solve());
 
     for(int i = 0; i < g_row; i++) {
         delete[] g_data[i];
