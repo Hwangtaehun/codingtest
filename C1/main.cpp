8487662c81d70f28e38bba644f1da24d7313459d
Hwangtaehun <r283@naver.com>
2025-08-11 20:41:54 +0900

    수정

--- C1/main.cpp
+++ C1/main.cpp
@@ -3,23 +3,11 @@
 using namespace std;
 
 int g_num, g_hap;
-int *g_data;
 
 void Input(){
     freopen("input.txt", "r", stdin);
     scanf("%d", &g_num);
     fclose(stdin);
-
-    int cnt = 0;
-
-    for(int i = 1; i <= g_num; i++){
-        if((g_num % i) == 0){
-            cnt++;
-        }
-    }
-
-    g_data = new int[cnt]();
-
 }
 
 void Output(){
@@ -29,17 +17,10 @@ void Output(){
 }
 
 void mySolve(){
-    int cnt = 0;
-
     for(int i = 1; i <= g_num; i++){
         if((g_num % i) == 0){
-            g_data[cnt] = i;
-            cnt++;
+            g_hap += i;
         }
-    }
-
-    for(int i = 0; i < cnt; i++){
-        g_hap += g_data[i];
     }
 }
 
