3a2038f7ca39cb24897929fcdecbbf2e071a91ca
Hwangtaehun <r283@naver.com>
2025-08-11 20:56:50 +0900

    c1 모범 답안 확인 완료

--- C1/main.cpp
+++ C1/main.cpp
@@ -24,10 +24,41 @@ void mySolve(){
     }
 }
 
+int Divisor1(){
+    int i, bound = g_num/2, tot = 1 + g_num;
+
+    for(i = 2; i <= bound; i++){
+        if( g_num%i == 0){
+            tot += i;
+        }
+    }
+
+    return tot;
+}
+
+int Divisor2(){
+    int i, tot = 0;
+
+    for(i = 1; i * i < g_num; i++){
+        if (g_num%i == 0){
+            tot += (i + g_num / i);
+        }
+    }
+
+    if(i*i == g_num){
+        tot += i;
+    }
+
+    return tot;
+}
+
 int main()
 {
     Input();
     mySolve();
+    printf("%d\n", Divisor1());
+    printf("%d\n", Divisor2());
+
     Output();
     return 0;
 }
