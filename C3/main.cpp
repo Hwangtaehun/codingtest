88936c79ffac8e217c7a6526fb6a213cf1e6cd22
Hwangtaehun <r283@naver.com>
2025-08-12 22:11:24 +0900

    C3완성

--- C3/main.cpp
+++ C3/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -21,35 +22,53 @@ void Print(vector<int> v){
     printf("\n");
 }
 
+void Print(set< vector<int> > results){
+    for (set< vector<int> >::iterator it = results.begin(); it != results.end(); ++it) {
+        vector<int> vec = *it;
+        for (vector<int>::iterator vit = vec.begin(); vit != vec.end(); ++vit) {
+            printf("%d ", *vit);
+        }
+        printf("\n");
+    }
+}
+
 void mySolve(){
-    int rest;
-    vector<int> m_v;
+    set< vector<int> > m_results;
 
     for(int i = 1; i < g_r; i++){
-        rest = g_r - i;
-        //printf("rest = %d\n", rest);
-        //printf("i=%d\n", i);
-        //Print(m_v);
-
+        int rest = g_r - i;
         for(int j = 1; j < rest; j++){
+            vector<int> m_v;
             int other = rest - j;
-            m_v.push_back(j);
-            //Print(m_v);
-            m_v.push_back(other);
-            //Print(m_v);
-            sort(m_v.begin(), m_v.end());
-            Print(m_v);
-
             m_v.push_back(i);
-            Print(m_v);
-            m_v.clear();
+            m_v.push_back(j);
+            m_v.push_back(other);
+            sort(m_v.begin(), m_v.end());
+            //Print(m_v);
+            m_results.insert(m_v);
         }
     }
+    //printf("\n");
+    //Print(m_results);
+
+    for (set< vector<int> >::iterator it = m_results.begin(); it != m_results.end(); ++it) {
+        const vector<int> &vec = *it;
+        if(vec[2] < vec[0] + vec[1]){
+            g_cnt++;
+        }
+    }
+}
+
+void Output(){
+    freopen("output.txt", "w", stdout);
+    printf("%d", g_cnt);
+    fclose(stdout);
 }
 
 int main()
 {
     Input();
     mySolve();
+    Output();
     return 0;
 }
