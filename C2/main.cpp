1ffde6f19e5110266537e03e71d2caffb5c1f280
Hwangtaehun <r283@naver.com>
2025-08-12 20:27:00 +0900

    코딩 완료

---
+++ C2/main.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define SIZE 9
+
+using namespace std;
+
+int r, c, big = 0;
+int data[SIZE][SIZE];
+
+void Print(){
+    for(int i = 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++){
+            printf("%2d ", data[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void Input(){
+    freopen("input.txt", "r", stdin);
+    for(int i = 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++){
+            scanf("%d", &data[i][j]);
+        }
+    }
+    fclose(stdin);
+}
+
+void Output(){
+    freopen("output.txt", "w", stdout);
+    printf("%d", big);
+    fclose(stdout);
+}
+
+int main()
+{
+    Input();
+    for(int i = 0; i < SIZE; i++){
+        for(int j = 0; j < SIZE; j++){
+            if(big < data[i][j]){
+                big = data[i][j];
+            }
+        }
+    }
+    Output();
+    return 0;
+}
