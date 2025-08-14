f80f67f77678eecf6c7b99446581e937ccdf0707
Hwangtaehun <r283@naver.com>
2025-08-14 19:53:37 +0900

    C6 다양한 모범 답안 풀이 방법

--- C6/main.cpp
+++ C6/main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 int g_data[SIZE + 2][SIZE + 2];
 int g_row, g_col, g_c;
+int g_dcol[4] = {1, 1, 0, -1}, g_drow[4] = {1, 0, 1, 1};
 
 void Input(){
     freopen("input.txt", "r", stdin);
@@ -69,6 +70,108 @@ void mySolve(){
     }
 }
 
+int search1(int color, int i, int j){
+    int cnt = 1;
+
+    while(color == g_data[i][j + 1]){
+        cnt++;
+        j++;
+    }
+
+    return cnt;
+}
+
+int search2(int color, int i, int j){
+    int cnt = 1;
+
+    while(color == g_data[i + 1][j + 1]){
+        cnt++;
+        i++, j++;
+    }
+
+    return cnt;
+}
+
+int search3(int color, int i, int j){
+    int cnt = 1;
+
+    while(color == g_data[i + 1][j]){
+        cnt++;
+        i++;
+    }
+
+    return cnt;
+}
+
+int search4(int color, int i, int j){
+    int cnt = 1;
+
+    while(color == g_data[i - 1][j + 1]){
+        cnt++;
+        i--, j++;
+    }
+
+    return cnt;
+}
+
+int Search(int color, int i, int j, int d){
+    int cnt = 0;
+
+    while(color == g_data[i][j]) {
+        i += g_dcol[d];
+        j += g_drow[d];
+        cnt++;
+    }
+
+    return cnt;
+}
+
+void Solve(){
+    int i, j;
+    for(i = 1; i <= 19; i++){
+        for(j = 1; j <= 19; j++){
+            if(g_data[i][j] != 0){
+                if(g_data[i][j-1] != g_data[i][j] && search1(g_data[i][j], i, j) == 5){
+                    printf("%d\n%d %d", g_data[i][j], i, j);
+                    return;
+                }
+
+                if(g_data[i-1][j-1] != g_data[i][j] && search2(g_data[i][j], i, j) == 5){
+                    printf("%d\n%d %d", g_data[i][j], i, j);
+                    return;
+                }
+
+                if(g_data[i-1][j] != g_data[i][j] && search3(g_data[i][j], i, j) == 5){
+                    printf("%d\n%d %d", g_data[i][j], i, j);
+                    return;
+                }
+
+                if(g_data[i+1][j-1] != g_data[i][j] && search4(g_data[i][j], i, j) == 5){
+                    printf("%d\n%d %d", g_data[i][j], i, j);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+void Other_Solve(){
+    int i, j;
+    for(i = 1; i <= 19; i++){
+        for(j = 1; j <= 19; j++){
+            if(g_data[i][j] != 0){
+                for(int d = 0; d < 4; d++){
+                    if(g_data[ i - g_dcol[d] ][ j - g_drow[d] ] !=  g_data[i][j] && Search(g_data[i][j], i, j, d) == 5){
+                        printf("%d\n%d %d", g_data[i][j], i, j);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    printf("0");
+}
+
 void Output(){
     freopen("output.txt", "w", stdout);
     printf("%d\n%d %d", g_c, g_col, g_row);
@@ -79,6 +182,9 @@ int main()
 {
     Input();
     mySolve();
+    Solve();
+    printf("\n");
+    Other_Solve();
     Output();
     return 0;
 }
