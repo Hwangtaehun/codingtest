cbcbd65e283be0d7b89d0c9abe75e76ba41a1b14
Hwangtaehun <r283@naver.com>
2025-08-15 21:18:53 +0900

    C8 다른 풀이

--- 다시풀어보기/C8/main.cpp
+++ 다시풀어보기/C8/main.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <queue>
 
 using namespace std;
+
+struct ELEMENT{
+    int temp;
+    int cnt;
+};
 
 int g_a, g_b, g_cnt = 0x7FFFFFFF;
 
@@ -16,7 +22,33 @@ void Output(){
     fclose(stdout);
 }
 
-void Search(int cur, int cnt){
+void Search(){
+    ELEMENT cur;
+    queue <ELEMENT> q;
+
+    q.push((ELEMENT){g_a,0});
+
+    while(!q.empty()){
+        cur = q.front();
+        q.pop();
+
+        if(cur.temp == g_b)
+            break;
+
+        if(cur.temp < g_b){
+            q.push((ELEMENT){cur.temp + 10, cur.cnt + 1});
+            q.push((ELEMENT){cur.temp + 5, cur.cnt + 1});
+            q.push((ELEMENT){cur.temp + 1, cur.cnt + 1});
+        }else{
+            q.push((ELEMENT){cur.temp - 10, cur.cnt + 1});
+            q.push((ELEMENT){cur.temp - 5, cur.cnt + 1});
+            q.push((ELEMENT){cur.temp - 1, cur.cnt + 1});
+        }
+    }
+    printf("bfs  = %d\n", cur.cnt);
+}
+
+void mySearch(int cur, int cnt){
     //이부분 풀이 참조
     if(cnt > g_cnt){
         return;
@@ -31,21 +63,22 @@ void Search(int cur, int cnt){
     }
 
     if(cur < g_b){
-        Search(cur + 1, cnt + 1);
-        Search(cur + 5, cnt + 1);
-        Search(cur + 10, cnt + 1);
+        mySearch(cur + 1, cnt + 1);
+        mySearch(cur + 5, cnt + 1);
+        mySearch(cur + 10, cnt + 1);
     }
     else{
-        Search(cur - 1, cnt + 1);
-        Search(cur - 5, cnt + 1);
-        Search(cur - 10, cnt + 1);
+        mySearch(cur - 1, cnt + 1);
+        mySearch(cur - 5, cnt + 1);
+        mySearch(cur - 10, cnt + 1);
     }
 }
 
 int main()
 {
     Input();
-    Search(g_a, 0);
+    mySearch(g_a, 0);
+    Search();
     Output();
     return 0;
 }
