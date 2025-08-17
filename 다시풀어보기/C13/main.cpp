1674a2b931550ca958ca048db0208f97aa48de33
Hwangtaehun <r283@naver.com>
2025-08-17 19:25:46 +0900

    C13 다른 모범답안 작성 완료
    input변경하고도 정확한 답이 나오는지 확인

--- 다시풀어보기/C13/main.cpp
+++ 다시풀어보기/C13/main.cpp
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <vector>
 
 using namespace std;
 //0:x, 1:block, -1:red
 int g_edge, g_node;
 int **g_data, *g_color;
+
+//모범답안
+int vertex, edge;
+int NodeColor[200];
+vector <int> data[200];
 
 void Print(){
     for(int i = 0; i < g_edge; i++){
@@ -18,6 +24,8 @@ void Input(){
     freopen("input.txt", "r", stdin);
     scanf("%d", &g_edge);
     scanf("%d", &g_node);
+    vertex = g_edge;
+    edge = g_node;
 
     g_data = new int*[g_edge];
     g_color = new int[g_node];
@@ -35,6 +43,8 @@ void Input(){
     for(int i = 0; i < g_node; i++){
         int a, b;
         scanf("%d %d", &a, &b);
+        data[a].push_back(b);
+        data[b].push_back(a);
         g_data[a][b] = g_data[b][a] = 1;
         g_color[i] = 0;
     }
@@ -60,6 +70,24 @@ void mySolve(int start, int color){
     }
 }
 
+void Solve(int v, int c){
+    NodeColor[v] = c;
+
+    for(int i = 0; i < data[v].size(); i++){
+        if(NodeColor[data[v][i]] == c){
+            NodeColor[v] = 0;
+            return;
+        }
+    }
+
+    for(int i = 0; i < data[v].size(); i++){
+        if(NodeColor[data[v][i]] == 0){
+            Solve(data[v][i], 1);
+            Solve(data[v][i], -1);
+        }
+    }
+}
+
 void Output(){
     freopen("output.txt", "w", stdout);
     for(int i = 0; i < g_edge; i++){
@@ -77,6 +105,14 @@ int main()
 {
     Input();
     mySolve(0, 1);
+    Solve(0, 1);
+    for(int i = 0; i < vertex; i++){
+        if(NodeColor[i] == 0){
+            printf("IMPOSSIBLE");
+            return 0;
+        }
+    }
+    printf("OK");
     Output();
     return 0;
 }
