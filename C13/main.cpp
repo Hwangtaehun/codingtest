8ce70ab526a79a8a5579d150029b204a1f491c6a
Hwangtaehun <r283@naver.com>
2025-08-17 15:56:51 +0900

    C13 완성
    모범답안 참고해서 완성

--- C13/main.cpp
+++ C13/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 //0:x, 1:block, -1:red
 int g_edge, g_node;
-int **g_data;
+int **g_data, *g_color;
 
 void Print(){
     for(int i = 0; i < g_edge; i++){
@@ -15,12 +15,13 @@ void Print(){
 }
 
 void Input(){
-    int color = 1;
     freopen("input.txt", "r", stdin);
     scanf("%d", &g_edge);
     scanf("%d", &g_node);
 
     g_data = new int*[g_edge];
+    g_color = new int[g_node];
+
     for(int i = 0; i < g_edge; i++){
         g_data[i] = new int[g_edge];
     }
@@ -34,16 +35,48 @@ void Input(){
     for(int i = 0; i < g_node; i++){
         int a, b;
         scanf("%d %d", &a, &b);
-        g_data[a][b] = g_data[b][a] = color;
-        color = -color;
+        g_data[a][b] = g_data[b][a] = 1;
+        g_color[i] = 0;
     }
 
     fclose(stdin);
 }
 
+void mySolve(int start, int color){
+    g_color[start] = color;
+
+    for(int i = 0; i < g_edge; i++){
+        if(g_data[start][i] && g_color[i] == color){
+            g_color[start] = 0;
+            return;
+        }
+    }
+
+    for(int i = 0; i < g_edge; i++){
+        if(g_data[start][i] && g_color[i] == 0){
+            mySolve(i, 1);
+            mySolve(i, -1);
+        }
+    }
+}
+
+void Output(){
+    freopen("output.txt", "w", stdout);
+    for(int i = 0; i < g_edge; i++){
+        if(g_color[i] == 0){
+            printf("IMPOSSIBLE");
+            fclose(stdout);
+            return;
+        }
+    }
+    printf("OK");
+    fclose(stdout);
+}
+
 int main()
 {
     Input();
-    //Print();
+    mySolve(0, 1);
+    Output();
     return 0;
 }
