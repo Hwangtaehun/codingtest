e2ce9259458e529803a1981c06ab36f00637ac6f
Hwangtaehun <r283@naver.com>
2025-08-17 14:29:05 +0900

    C12 모범답안 확인 완료

--- C12/main.cpp
+++ C12/main.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 #include <utility>
 #include <vector>
-#include <malloc.h>
+#include <queue>
 
 using namespace std;
 
@@ -11,9 +12,35 @@ int g_dy[4] = {0, 0, 1, -1};
 int g_row, g_col, g_cnt = 0;
 int **g_data;
 
+struct ROWCOL {
+    int row;
+    int col;
+};
+
+int a1[101][101], a2[101][101];
+int rowCnt, colCnt;
+queue <ROWCOL> q;
+
+void ArrayPrn(){
+    for(int i = 1;  i <= rowCnt; i++){
+        for(int j = 1; j <= colCnt; j++){
+            printf("%d ", a1[i][j]);
+        }
+        printf("\t");
+        for(int j = 1; j <= colCnt; j++){
+            printf("%d ", a2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 void Input(){
     freopen("input.txt", "r", stdin);
     scanf("%d %d", &g_col, &g_row);
+    rowCnt = g_col;
+    colCnt = g_row;
+
     g_data = (int **)malloc(g_col * sizeof(int *));
 
     for(int i = 0; i < g_col; i++){
@@ -23,6 +50,7 @@ void Input(){
     for(int i = 0; i < g_col; i++){
         for(int j = 0; j < g_row; j++){
             scanf("%d", &g_data[i][j]);
+            a2[i+1][j+1] = a1[i+1][j+1] = g_data[i][j];
         }
     }
 
@@ -95,9 +123,196 @@ void Output(){
     fclose(stdout);
 }
 
+void init_array(){
+    for(int i = 1; i < rowCnt; i++){
+        for(int j = 1; j < colCnt; j++){
+            a2[i][j] = a1[i][j] = 0;
+        }
+    }
+
+    for(int i = 0; i < g_col; i++){
+        for(int j = 0; j < g_row; j++){
+            a2[i+1][j+1] = a1[i+1][j+1] = g_data[i][j];
+        }
+    }
+}
+
+void QueuePrn(queue <ROWCOL> q1){
+    while(!q1.empty()){
+        printf("(%d,%d) ", q1.front().row, q1.front().col);
+        q1.pop();
+    }
+    printf("\n");
+}
+
+void Fill1(int x, int y){
+    if( x < 1 || x > rowCnt || y < 1 || y > colCnt || a1[x][y] != 0)
+        return;
+    a1[x][y] = 2;
+    Fill1(x + 1, y);
+    Fill1(x - 1, y);
+    Fill1(x, y + 1);
+    Fill1(x, y - 1);
+}
+
+void Fill2(int x, int y){
+    if( x < 1 || x > rowCnt || y < 1 || y > colCnt || a2[x][y] != 0)
+        return;
+    a2[x][y] = 2;
+    Fill2(x + 1, y);
+    Fill2(x - 1, y);
+    Fill2(x, y + 1);
+    Fill2(x, y - 1);
+}
+
+void Fill3(int x, int y){
+    if(x < 1 || x > rowCnt || y < 1 || y > colCnt)
+        return;
+
+    if(a2[x][y] == 3 || a2[x][y] == 0){
+        a2[x][y] = 2;
+
+        Fill3(x + 1, y);
+        Fill3(x - 1, y);
+        Fill3(x, y + 1);
+        Fill3(x, y - 1);
+    }
+}
+
+int SideCount(int x, int y){
+    int cnt = 0;
+
+    if(a1[x + 1][y] == 2)
+        cnt++;
+    if(a1[x - 1][y] == 2)
+        cnt++;
+    if(a1[x][y + 1] == 2)
+        cnt++;
+    if(a1[x][y - 1] == 2)
+        cnt++;
+
+    return cnt;
+}
+
+int SideCount2(int x, int y){
+    int cnt = 0;
+
+    if(a2[x + 1][y] == 2)
+        cnt++;
+    if(a2[x - 1][y] == 2)
+        cnt++;
+    if(a2[x][y + 1] == 2)
+        cnt++;
+    if(a2[x][y - 1] == 2)
+        cnt++;
+
+    return cnt;
+}
+
+void Solve1(){
+    int i, j, hour = 0, flag;
+    printf("Solve1 process\n");
+    while(1) {
+        Fill1(1,1);
+        flag = 0;
+        for(i = 1; i <= rowCnt; i++){
+            for(j = 1; j <= colCnt; j++){
+                if(a1[i][j] == 1 && SideCount(i, j) >= 2){
+                    a2[i][j] = 0;
+                    flag = 1;
+                }
+            }
+        }
+
+        if(flag == 0){
+            printf("hour = %d\n", hour);
+            break;
+        }
+        hour++;
+        ArrayPrn();
+        memcpy((void *)a1, (void *)a2, sizeof(a2));
+    }
+}
+
+void Solve2(){
+    int i, j, hour = 0, flag;
+    printf("Solve2 process\n");
+    Fill1(1,1);
+    Fill2(1,1);
+    ArrayPrn();
+    printf("aaaa\n");
+    while(1){
+        flag = 0;
+        for(i = 1; i <= rowCnt; i++){
+            for(j = 1; j <= colCnt; j++){
+                if(a1[i][j] == 1 && SideCount(i, j) >= 2){
+                    a2[i][j] = 0;
+                    flag = 1;
+                }
+            }
+        }
+        ArrayPrn();
+        if(flag == 0){
+            printf("hour = %d\n", hour);
+            break;
+        }
+
+        for(int i = 1; i <= rowCnt; i++){
+            for(int j = 1; j <= colCnt; j++){
+                if(a1[i][j] == 1 && a2[i][j] == 0){
+                    Fill2(i, j);
+                }
+            }
+        }
+        hour++;
+        memcpy((void *)a1, (void *)a2, sizeof(a2));
+    }
+}
+
+void Solve3(){
+    int i, j, hour = 0, flag;
+    printf("Solve3 process\n");
+    Fill3(1,1);
+    ArrayPrn();
+    printf("aaaa\n");
+    while(1) {
+        flag = 0;
+        for(i = 1; i <= rowCnt; i++){
+            for(j = 1; j <= colCnt; j++){
+                if(a1[i][j] == 1 && SideCount2(i, j) >= 2){
+                    a2[i][j] = 3;
+                    q.push((ROWCOL){i, j});
+                    flag = 1;
+                }
+            }
+        }
+        ArrayPrn();
+        if(flag == 0) {
+            printf("hour = %d\n", hour);
+            break;
+        }
+
+        ROWCOL cur;
+        QueuePrn(q);
+
+        while(!q.empty()){
+            cur = q.front();
+            q.pop();
+            Fill3(cur.row, cur.col);
+        }
+        hour++;
+        memcpy((void *)a1, (void *)a2, sizeof(a2));
+    }
+}
+
 int main()
 {
     Input();
+    Solve1();
+    init_array();
+    Solve2();
+    init_array();
+    Solve3();
     Solve();
     Output();
     return 0;
