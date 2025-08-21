7995a66dda0a48aa1e78f2e286f188e160e1ed62
Hwangtaehun <r283@naver.com>
2025-08-21 15:56:34 +0900

    C22완성
    모범답안 확인하기

--- C22/main.cpp
+++ C22/main.cpp
@@ -1,9 +1,10 @@
 #include <cstdio>
 #include <algorithm>
-#define SIZE 2
+#define SIZE 3
 
 using namespace std;
 
+//[0]-start, [1]-goal, [2]-block
 int g_size, **g_data, g_cnt;
 
 void Input()
@@ -19,15 +20,16 @@ void Input()
 
     for(int i = 0; i < g_size; i++){
         scanf("%d %d", &g_data[i][0], &g_data[i][1]);
+        g_data[i][2] = 0;
     }
     fclose(stdin);
 }
 
 void Output()
 {
-    //freopen("output.txt", "w", stdout);
+    freopen("output.txt", "w", stdout);
     printf("%d", g_cnt);
-    //fclose(stdout);
+    fclose(stdout);
 }
 
 bool Asc(int* a, int* b){
@@ -37,16 +39,90 @@ bool Asc(int* a, int* b){
 void Testprint()
 {
     for(int i = 0; i < g_size; i++){
-        printf("%d %d\n", g_data[i][0], g_data[i][1]);
+        printf("%2d %2d %2d\n", g_data[i][0], g_data[i][1], g_data[i][2]);
     }
+    printf("\n");
+}
+
+int block_count(int start, int arrive){
+    int m_cnt = 0;
+
+    if(start < arrive){
+       for(int i = 0; i < g_size; i++){
+            if(g_data[i][2] != -1){
+                if(start < g_data[i][0]){
+                    if(arrive > g_data[i][1]){
+                        m_cnt++;
+                    }
+                }
+            }
+        }
+    }else{
+        for(int i = 0; i < g_size; i++){
+            if(g_data[i][2] != -1){
+                if(start > g_data[i][0]){
+                    if(arrive < g_data[i][1]){
+                        m_cnt++;
+                    }
+                }
+            }
+        }
+    }
+
+    return m_cnt;
+}
+
+bool Check(){
+    for(int i = 0; i < g_size; i++){
+        if(g_data[i][2] > 0){
+            //printf("-----\n%d %d\n-----\n", g_data[i][0], g_data[i][1]);
+            return false;
+        }
+    }
+
+    return true;
 }
 
 void Solve(){
+    while(1){
+        int m_index, m_big = 0;
+
+        for(int i = 0; i < g_size; i++){
+            if(g_data[i][2] != -1){
+                int m_start, m_arrive;
+                m_start = g_data[i][0];
+                m_arrive = g_data[i][1];
+                g_data[i][2] = block_count(m_start, m_arrive);
+            }
+        }
+
+        //Testprint();
+
+        if(Check()){
+            return;
+        }
+
+        for(int i = 0; i < g_size; i++){
+            if(m_big < g_data[i][2]){
+                //printf("i = %d\n", i);
+                m_big = g_data[i][2];
+                m_index = i;
+            }
+        }
+
+        g_data[m_index][2] = -1;
+        g_cnt++;
+
+        //Testprint();
+    }
 }
 
 int main()
 {
     Input();
     sort(g_data, g_data + g_size, Asc);
+    Testprint();
+    Solve();
+    Output();
     return 0;
 }
