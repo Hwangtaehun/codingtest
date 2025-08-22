8b5b1d75e0e6e78917bb9d014eea775878e3a73b
Hwangtaehun <r283@naver.com>
2025-08-22 21:49:23 +0900

    C25 모범답안 확인 완료
    다른 점은 기름 기준과 거리 기준 차이

--- C25/main.cpp
+++ C25/main.cpp
@@ -8,10 +8,20 @@ using namespace std;
 int g_place, g_trans, g_gas = 0x0fffffff, **g_data;
 bool *g_visitd;
 
+//solve
+int n, k, visit[11], dmin=0x0fffffff;
+struct airport{
+    int x;
+    int y;
+} a[11];
+
 void Input()
 {
     freopen("input.txt", "r", stdin);
     scanf("%d %d", &g_place, &g_trans);
+    n = g_place;
+    k = g_trans;
+
     g_data = new int*[g_place];
     g_visitd = new bool[g_place];
 
@@ -20,11 +30,22 @@ void Input()
         g_visitd[i] = false;
     }
 
+    a[0].x = 0, a[0].y = 0;
+
     for(int i = 0; i < g_place; i++){
         scanf("%d %d", &g_data[i][0], &g_data[i][1]);
+        a[i+1].x = g_data[i][0];
+        a[i+1].y = g_data[i][1];
     }
-
     fclose(stdin);
+
+    a[n+1].x = 10000, a[n+1].y = 10000;
+    visit[0] = 1;
+}
+
+int Distance(int p1, int p2)
+{
+    return (a[p1].x - a[p2].x) * (a[p1].x - a[p2].x) + (a[p1].y - a[p2].y) * (a[p1].y - a[p2].y);
 }
 
 int Lqude(int s_x, int s_y, int g_x, int g_y){
@@ -39,32 +60,52 @@ void TestPrint(){
     }
 }
 
-void mySolve(int cnt, int gas, int x, int y){
-    if(cnt >= g_trans){
-        if(cnt == g_trans){
-            if(gas < Lqude(x, y, 10000, 10000)){
-                gas = Lqude(x, y, 10000, 10000);
-            }
+void Solve(int p, int c, int d){
+    int d2;
+    if(c == k){
+        d2 = Distance(p, n + 1);
+        if(d < d2)
+            d = d2;
+        if(dmin > d)
+            dmin = d;
+        return;
+    }
 
-            if(gas < g_gas){
-                g_gas = gas;
-            }
+    for(int i = 0; i <= n+1; i++){
+        if(visit[i] == 0){
+            visit[i] = 1;
+            d2 = Distance(p, i);
+            if(d < d2)
+                d = d2;
+            Solve(i, c+1, d);
+            visit[i] = 0;
+        }
+    }
+}
+
+void mySolve(int cnt, int gas, int x, int y){
+    if(cnt == g_trans){
+        if(gas < Lqude(x, y, 10000, 10000)){
+            gas = Lqude(x, y, 10000, 10000);
+        }
+
+        if(gas < g_gas){
+            g_gas = gas;
         }
         return;
     }
 
-    //while(1){
-        for(int i = 0; i < g_place; i++){
-            if(!g_visitd[i]){
-                g_visitd[i] = true;
-                if(gas < Lqude(x, y, g_data[i][0], g_data[i][1])){
-                    gas = Lqude(x, y, g_data[i][0], g_data[i][1]);
-                }
-                mySolve(cnt + 1, gas, g_data[i][0], g_data[i][1]);
-                g_visitd[i] = false;
+
+    for(int i = 0; i < g_place; i++){
+        if(!g_visitd[i]){
+            g_visitd[i] = true;
+            if(gas < Lqude(x, y, g_data[i][0], g_data[i][1])){
+                gas = Lqude(x, y, g_data[i][0], g_data[i][1]);
             }
+            mySolve(cnt + 1, gas, g_data[i][0], g_data[i][1]);
+            g_visitd[i] = false;
         }
-    //}
+    }
 }
 
 void Output()
@@ -79,6 +120,8 @@ int main()
     Input();
     //TestPrint();
     mySolve(0, 0, 0, 0);
+    Solve(0, 0, 0);
+    printf("%.f\n", ceil(sqrt(dmin)/10));
     Output();
     return 0;
 }
