329158262ec4fb9499ec899995db6a56fc823a17
Hwangtaehun <r283@naver.com>
2025-08-22 14:18:18 +0900

    C23 모범답안 첫번째 방법

--- C23/main.cpp
+++ C23/main.cpp
@@ -1,21 +1,37 @@
 #include <cstdio>
+#include <cmath>
+
 #define SIZE 2
 
 using namespace std;
 
 int **g_data, g_size, g_case, g_dis = 0;
 
+//solve
+int data[1010][2], n, m, ans = 0x0fffffff;
+
+int dis(int a, int b){
+    int d = abs(data[a][0] - data[b][0]) + abs(data[a][1] - data[b][1]);
+    return d;
+}
+
 void Input(){
     freopen("input.txt", "r", stdin);
     scanf("%d %d", &g_size, &g_case);
+    n = g_size, m = g_case;
 
     g_data = new int*[g_case];
     for(int i = 0; i < g_case; i++){
         g_data[i] = new int[SIZE];
     }
 
+    data[0][0] = data[0][1] = 1;
+    data[1][0] = data[1][1] = n;
+
     for(int i = 0; i < g_case; i++){
         scanf("%d %d", &g_data[i][0], &g_data[i][1]);
+        data[i+2][0] = g_data[i][0];
+        data[i+2][1] = g_data[i][1];
     }
 
     fclose(stdin);
@@ -75,10 +91,26 @@ void mySolve(){
     }
 }
 
+void solve(int a, int b, int d){
+    int next = (a > b ? a : b) + 1;
+    printf("%d,%d,%d,%d: %d,%d\n", a, b, next, d, dis(a, next), dis(b, next));
+
+    if(next >= m + 2){
+        if(d < ans)
+            ans = d;
+        return;
+    }
+
+    solve(next, b, d + dis(a, next));
+    solve(a, next, d + dis(b, next));
+}
+
 int main()
 {
     Input();
     //TestPrint();
+    solve(0, 1, 0);
+    printf("%d", ans);
     mySolve();
     Output();
     return 0;
