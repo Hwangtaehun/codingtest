20526706c9433a7608e14c447c28c7a764be097f
Hwangtaehun <r283@naver.com>
2025-08-25 20:09:03 +0900

    C27완성

--- C27/main.cpp
+++ C27/main.cpp
@@ -29,6 +29,50 @@ void Testprint(){
     }
 }
 
+int Possible(int num){
+    for(int i = 0; i < g_size; i++){
+        if(g_island[i][0] == num){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void mySolve(int index, int hang){
+    if(index == g_size - 1){
+        hang = 1;
+    }
+
+    if(index == 0 && hang == 1){
+        g_cnt++;
+        return;
+    }
+
+    int m_cur = g_island[index][0];
+    int m_long = g_island[index][1];
+    int m_index = -1;
+
+    if(hang){
+        for(int i = m_cur - 1; i >= m_cur - m_long; i--){
+            m_index = Possible(i);
+            if((m_index != -1 && g_island[m_index][2]) &&!g_visited[m_index]){
+                g_visited[m_index] = true;
+                mySolve(m_index, hang);
+                g_visited[m_index] = false;
+            }
+        }
+    }else{
+        for(int i = m_cur + 1; i <= m_cur + m_long; i++){
+            m_index = Possible(i);
+            if(m_index != -1 && !g_visited[m_index]){
+                g_visited[Possible(i)] = true;
+                mySolve(m_index, hang);
+                g_visited[Possible(i)] = false;
+            }
+        }
+    }
+}
+
 void Output(){
     int m_result = g_cnt % REST;
     freopen("output.txt", "w", stdout);
@@ -39,6 +83,8 @@ void Output(){
 int main()
 {
     Input();
-    Testprint();
+    //Testprint();
+    mySolve(0, 0);
+    Output();
     return 0;
 }
