b3cea9eb2565f6f2b80cf7d802c1b3a725b2622b
Hwangtaehun <r283@naver.com>
2025-08-25 21:56:46 +0900

    D2 모범답안 확인 완료

--- D2/main.cpp
+++ D2/main.cpp
@@ -1,8 +1,12 @@
 #include <cstdio>
+#include <ctime>
 
 using namespace std;
 
 int g_num, g_result;
+
+//solve
+int n;
 
 void Input(){
     freopen("input.txt", "r", stdin);
@@ -39,10 +43,57 @@ void mySolve(){
     }
 }
 
+bool isPrime(int k){
+    for(int i = 2; i * i <= k; i++){
+        if(k % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void Prime1(){
+    int i, cnt = 0;
+    for(int i = 2; i < 100000; i++){
+        if(isPrime(i)){
+            cnt++;
+            if(cnt == n)
+                break;
+        }
+    }
+    printf("%d\n", i);
+}
+
+void Prime2(){
+    int i, j, num, array[100001] = {0}, cnt = 0;
+    for(i = 2; i < 100000; i++){
+        array[i] = i;
+    }
+    for(i = 2; i <= 100000; i++){
+        if(array[i] != 0){
+            cnt++;
+            if(cnt >= n)
+                break;
+            for(j = i + i; j <= 100000; j += i){
+                array[i] = 0;
+            }
+        }
+    }
+    printf("%d\n", i);
+}
+
 int main()
 {
-    Input();
-    mySolve();
-    Output();
+    //Input();
+    //mySolve();
+    //Output();
+
+    scanf("%d", &n);
+    printf("clock()\t = %d\n", clock());
+
+    Prime1();
+    printf("clock()\t = %d\n", clock());
+
+    Prime2();
+    printf("clock()\t = %d\n", clock());
     return 0;
 }
