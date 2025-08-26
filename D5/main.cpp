c3de1bc17b4566751abbc0ca9ee107c25704af64
Hwangtaehun <r283@naver.com>
2025-08-26 21:46:00 +0900

    D5 모범답안 확인

--- D5/main.cpp
+++ D5/main.cpp
@@ -12,9 +12,13 @@ struct Tri{
 vector <Tri> g_data;
 int g_num;
 
+//solve
+int n;
+
 void Input(){
     freopen("input.txt", "r", stdin);
     scanf("%d", &g_num);
+    n = g_num;
     fclose(stdin);
 }
 
@@ -45,9 +49,40 @@ void mySolve(){
     }
 }
 
+int Solve2(){
+    int c, cnt = 0;
+    for(int a = 1; a <= n; a++){
+        for(int b = a; b <= n; b++){
+            c = n - (a + b);
+            if(c < a + b && (b <= c)){
+                printf("%d %d %d\n", a, b, c);
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+int Solve3(){
+    int a, b, c, cnt = 0;
+    for(c = n/3; c <= n/2; c++){
+        for(a = 1; a <= n/3; a++){
+            b = n - (a + c);
+
+            if(c < a + b && (a <= b && b <= c)){
+                printf("%d %d %d\n", a, b, c);
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
 int main()
 {
     Input();
+    printf("%d\n", Solve2());
+    printf("%d\n", Solve3());
     mySolve();
     Output();
     return 0;
