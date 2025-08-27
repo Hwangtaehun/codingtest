347f3dd436403ae5c04289f0b2439c06356f1d97
Hwangtaehun <r283@naver.com>
2025-08-27 21:19:37 +0900

    D8 모범답안 확인

--- D8/main.cpp
+++ D8/main.cpp
@@ -6,13 +6,18 @@ using namespace std;
 int g_money, g_size, g_min = 0x7fffffff;
 vector<int> g_coin;
 
+//solve
+int n, m, debugCnt, coin[10], ans = 0x7FFFFFFF;
+
 void Input(){
     freopen("input.txt", "r", stdin);
     scanf("%d %d", &g_money, &g_size);
+    m = g_money;
+    n = g_size;
+
     for(int i = 0; i < g_size; i++){
-        int n;
-        scanf("%d", &n);
-        g_coin.push_back(n);
+        scanf("%d", coin + i);
+        g_coin.push_back(coin[i]);
     }
     fclose(stdin);
 }
@@ -40,9 +45,78 @@ void mySolve(int money, int index, int cnt){
     }
 }
 
+int Greedy(int mon){
+    int cnt = 0;
+    for(int i = n-1; i >= 0; i--){
+        cnt += mon/coin[i];
+        mon %= coin[i];
+        printf("mon=%d, coin=%d, cnt=%d\n", mon, coin[i], cnt);
+    }
+
+    if(mon == 0){
+        ans = cnt;
+    }
+
+    return mon;
+}
+
+void Solve1(int mon, int cnt){
+    if(cnt > ans)
+        return;
+
+    if(mon > m)
+        return;
+
+    debugCnt++;
+
+    if(mon == m){
+        if(cnt < ans)
+            ans = cnt;
+        return;
+    }
+
+    for(int i = 0; i < n; i++){
+        Solve1(mon + coin[i], cnt + 1);
+    }
+}
+
+//기존과 같음
+void Solve2(int mon, int k, int cnt){
+    if(cnt > ans)
+        return;
+
+    if(k == n || mon > m)
+        return;
+
+    debugCnt++;
+
+    if(mon == m){
+        printf("mon = %d, k = %d, cnt = %d\n", mon, k, cnt);
+
+        if(ans > cnt){
+            ans = cnt;
+        }
+
+        return;
+    }
+
+    for(int i = 0; mon + coin[k] * i <= m; i++){
+        Solve2(mon + coin[k] * i, k + 1, cnt + i);
+    }
+}
+
 int main()
 {
     Input();
+
+    if(Greedy(m) != 0){
+        Solve1(0, 0);
+        //Solve2(0, 0, 0);
+    }
+
+    printf("%d\n", ans);
+    printf("debugCnt = %d\n", debugCnt);
+
     mySolve(g_money, 0, 0);
     Output();
     return 0;
