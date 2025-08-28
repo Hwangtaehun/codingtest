76a7e2560bb999227e6001c4d08af0e978f445d1
Hwangtaehun <r283@naver.com>
2025-08-28 19:50:09 +0900

    D10 모범답안 확인 완료

--- D10/main.cpp
+++ D10/main.cpp
@@ -1,18 +1,24 @@
 #include <cstdio>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> g_data, g_result;
 int g_size, g_dis = 0x7FFFFFFF, g_fir = 0x7FFFFFFF;
 
+//solve
+int gift[20], g1, g2, g3, n, d = 0x0fffffff, gd = 0x0fffffff, sum, debugCnt;
+
 void Input(){
     freopen("input.txt", "r", stdin);
     scanf("%d", &g_size);
+    n = g_size;
+
     for(int i = 0; i < g_size; i++){
-        int n;
-        scanf("%d", &n);
-        g_data.push_back(n);
+        scanf("%d", &gift[i]);
+        g_data.push_back(gift[i]);
+        sum += gift[i];
     }
     fclose(stdin);
 }
@@ -66,9 +72,53 @@ void mySolve(int a, int b, int c, int cnt){
     mySolve(a, b, c + g_data[cnt], cnt + 1);
 }
 
+void ArrayPrn(){
+    for(int i = 0; i < n; i++){
+        printf("%d ", gift[i]);
+    }
+
+    printf("sum = %d\n", sum);
+}
+
+bool Desc(int left, int right){
+    return left > right;
+}
+
+void Solve(int no, int a, int b, int c){
+    if(c > sum / 3 || b > sum / 2)
+        return;
+
+    int restSum = sum - (a + b + c);
+    if((restSum + a) < b || (restSum + b) < c)
+        return;
+
+    if(a - (c + restSum) > d)
+        return;
+    debugCnt++;
+
+    if(no < n){
+        Solve(no + 1, a, b, c + gift[no]);
+        Solve(no + 1, a, b + gift[no], c);
+        Solve(no + 1, a + gift[no], b, c);
+    }
+    else if(a >= b && b >= c && d >= (a-c) && gd > a){
+        g1 = a, g2 = b, g3 = c;
+        d = a - c;
+        gd = a;
+        printf("%d %d %d %d\n", g1, g2, g3, d);
+    }
+}
+
 int main()
 {
     Input();
+
+    sort(gift, gift + n, Desc);
+    ArrayPrn();
+    Solve(0, 0, 0, 0);
+    printf("%d %d %d\n", g1, g2, g3);
+    printf("debugCnt = %d\n", debugCnt);
+
     mySolve(0, 0, 0, 0);
     Output();
     return 0;
