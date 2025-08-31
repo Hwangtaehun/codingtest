f2b08d4eaaaa164ff77d21a3cfa0b9a65fe156df
Hwangtaehun <r283@naver.com>
2025-08-31 21:38:13 +0900

    E1 완성

---
+++ E1/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> g_data;
+int g_result;
+
+void Input(){
+    string m_str;
+
+    freopen("input.txt", "r", stdin);
+    cin >> m_str;
+    fclose(stdin);
+
+    for(size_t i = 0; i < m_str.size(); i++) {
+        g_data.push_back((int)(m_str[i] - '0'));
+    }
+}
+
+void Print(){
+    for(int i = 0; i < g_data.size(); i++){
+        printf("%d", g_data[i]);
+    }
+    printf("\n");
+}
+
+void Output(){
+    freopen("output.txt","w", stdout);
+    printf("%d", g_result);
+    fclose(stdout);
+}
+
+void mySolve(){
+    int num = 0;
+    for(int i = g_data.size() - 1; i >= 0; i--){
+        num = num * 10 + g_data[i];
+    }
+
+    g_result = num;
+}
+
+int main()
+{
+    Input();
+    mySolve();
+    Output();
+    return 0;
+}
