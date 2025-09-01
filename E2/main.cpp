a1513bc9e1221ddfb1cb043a7d51c638aea81767
Hwangtaehun <r283@naver.com>
2025-09-01 19:45:11 +0900

    E2 모범답안 확인

--- E2/main.cpp
+++ E2/main.cpp
@@ -8,6 +8,10 @@ using namespace std;
 vector<string> g_result;
 int g_size;
 
+//solve
+#define MAX 10000
+char str[MAX + 1];
+
 void Input(){
     freopen("input.txt", "r", stdin);
     scanf("%d", &g_size);
@@ -37,9 +41,45 @@ void mySolve(){
     }
 }
 
+void Solve1(int n){
+    if(n == 1){
+        putchar('*');
+        putchar('\n');
+    }else{
+        Solve1(n - 1);
+        for(int i = 0; i < n; i++){
+            putchar('*');
+        }
+        putchar('\n');
+    }
+}
+
+void Solve2(int n){
+    if(n > 0){
+        Solve2(n - 1);
+        for(int i = 0; i < n; i++){
+            putchar('*');
+        }
+        putchar('\n');
+    }
+}
+
+void Solve3(int n){
+    if(n > 0){
+        Solve3(n - 1);
+        str[n] = '*';
+        printf("%s\n", str + 1);
+    }
+}
+
 int main()
 {
+    int n;
     Input();
+    n = g_size;
+    Solve1(n);
+    Solve2(n);
+    Solve3(n);
     mySolve();
     Output();
     return 0;
