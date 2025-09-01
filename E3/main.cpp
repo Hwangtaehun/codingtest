d2de0b74159699e7f48acfa770ee057043bba753
Hwangtaehun <r283@naver.com>
2025-09-01 21:15:06 +0900

    E3 모범답안 확인

--- E3/main.cpp
+++ E3/main.cpp
@@ -28,9 +28,65 @@ void mySolve(){
     g_res = myFactorial(g_n) / (myFactorial(g_n - g_r) * myFactorial(g_r));
 }
 
+void Solve1(int n, int k){
+    int i, p = 1, q = 1, r = 1;
+
+    for(i = 1; i <= n; i++){
+        p = p * i;
+    }
+
+    for(i = 1; i <= k; i++){
+        q = q * i;
+    }
+
+    for(i = 1; i <= n - k; i++){
+        r = r * i;
+    }
+
+    printf("%dC%d = %d\n", n, k, p / (q * r));
+
+    p = 1;
+    for(i = n - k + 1; i <= n; i++){
+        p = p * i;
+    }
+    printf("%dC%d = %d\n", n, k, p / q);
+
+    p = 1;
+    for(i = 1; i <= k; i++){
+        p = p * (n - i + 1) / i;
+    }
+    printf("%dC%d = %d\n", n, k, p);
+}
+
+int Solve2(int n, int k){
+    if(k == n){
+        return 1;
+    }else if(k == 1){
+        return n;
+    }else{
+        return Solve2(n - 1, k - 1) + Solve2(n - 1, k);
+    }
+}
+
+int Solve3(int n, int k){
+    if(k == n){
+        return 1;
+    }else if(k == 1){
+        return n;
+    }else{
+        return Solve3(n, k - 1) * (n - k + 1) / k;
+    }
+}
+
 int main()
 {
+    int n, k;
     Input();
+    n = g_n;
+    k = g_r;
+    Solve1(n, k);
+    printf("%d\n", Solve2(n, k));
+    printf("%d\n", Solve3(n, k));
     mySolve();
     Output();
     return 0;
