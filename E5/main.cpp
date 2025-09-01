45d7fcf0af80c80d9ba6f8e6f2ca959a2369ca7f
Hwangtaehun <r283@naver.com>
2025-09-01 22:01:07 +0900

    E5완성
    E4와 E5 둘 다 모범답안 확인하기

---
+++ E5/main.cpp
@@ -0,0 +1,78 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int g_s, g_e, g_dis;
+
+void Input(){
+    freopen("input.txt", "r", stdin);
+    scanf("%d %d", &g_s, &g_e);
+    fclose(stdin);
+}
+
+void Output(){
+    freopen("output.txt", "w", stdout);
+    printf("%d", g_dis);
+    fclose(stdout);
+}
+
+int Small(int a, int b){
+    return a < b ? a : b;
+}
+
+void mySolve(){
+    vector<int> m_start, m_end;
+
+    int m_temp = g_s;
+
+    while(m_temp > 0){
+        m_start.push_back(m_temp);
+        m_temp /= 2;
+    }
+
+    m_temp = g_e;
+
+    while(m_temp > 0){
+        m_end.push_back(m_temp);
+        m_temp /= 2;
+    }
+
+    sort(m_start.begin(), m_start.end());
+    sort(m_end.begin(), m_end.end());
+
+    int m_sma = Small(m_start.size(), m_end.size());
+    int m_pa = 0;
+
+    for(int i = 0; i < m_sma; i++){
+        if(m_start[i] == m_end[i]){
+            m_pa = m_start[i];
+        }
+    }
+
+    reverse(m_start.begin(), m_start.end());
+    reverse(m_end.begin(), m_end.end());
+
+    for(int i = 0; i < m_start.size(); i++){
+        if(m_start[i] == m_pa){
+            g_dis += i;
+            break;
+        }
+    }
+
+    for(int i = 0; i < m_end.size(); i++){
+        if(m_end[i] == m_pa){
+            g_dis += i;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    Input();
+    mySolve();
+    Output();
+    return 0;
+}
