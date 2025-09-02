0ab6534eb8fbca05144683373147f9490f0e70fe
Hwangtaehun <r283@naver.com>
2025-09-02 21:53:39 +0900

    E7 완성

--- E7/main.cpp
+++ E7/main.cpp
@@ -12,21 +12,56 @@ void Input(){
     scanf("%d", &g_size);
     for(int i = 0; i < g_size; i++){
         int temp;
-        scanf("%d", &temp);
+        scanf("%1d", &temp);
         g_data.push_back(temp);
     }
     fclose(stdin);
 }
 
 void Output(){
-    freopen("output.txt", "w", stout);
+    freopen("output.txt", "w", stdout);
     for(int i = 0; i < g_result.size(); i++){
         printf("%c", g_result[i]);
     }
     fclose(stdout);
 }
 
+void Print(){
+    for(int i = 0; i < g_size; i++){
+        printf("%d", g_data[i]);
+    }
+}
+
+bool Check(int x, int range){
+    for(int i = x + 1; i < x + range; i++){
+        if(g_data[i] != g_data[x]){
+            return false;
+        }
+    }
+    return true;
+}
+
+void mySolve(int x, int range){
+    if(range ==  1){
+        g_result.push_back(g_data[x] + '0');
+        return;
+    }
+
+    if(Check(x, range)){
+        g_result.push_back(g_data[x] + '0');
+        return;
+    }else{
+        g_result.push_back('-');
+        mySolve(x, range / 2);
+        mySolve(x + range / 2 , range / 2);
+    }
+}
+
 int main()
 {
+    Input();
+    //Print();
+    mySolve(0, g_size);
+    Output();
     return 0;
 }
