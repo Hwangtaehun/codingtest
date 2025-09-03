2535b294bb3dcfc1cc003b3b360eeabbfd4675a2
Hwangtaehun <r283@naver.com>
2025-09-03 20:32:52 +0900

    E8 수정
    문제점이 있어서 수정

--- E8/main.cpp
+++ E8/main.cpp
@@ -1,10 +1,12 @@
 #include <cstdio>
 #include <vector>
 #include <cstdlib>
+#include <queue>
 
 using namespace std;
 
 int g_size;
+bool *g_check;
 vector<char> g_data;
 vector<int> g_result;
 
@@ -18,6 +20,13 @@ void Input(){
     for(int i = 0; m_str[i] != '\0'; i++){
         g_data.push_back(m_str[i]);
     }
+
+    g_check = new bool[g_data.size()];
+
+    for(int i = 0; i < g_data.size(); i++){
+        g_check[i] = false;
+    }
+
 }
 
 void Output(){
@@ -28,23 +37,37 @@ void Output(){
     fclose(stdout);
 }
 
+bool Check(){
+    for(int i = 0; i < g_data.size(); i++){
+        if(!g_check[i])
+            return false;
+    }
+    return true;
+}
+
 void Print(){
     for(int i = 0; i < g_data.size(); i++){
         printf("%c", g_data[i]);
     }
 }
 
-void mySolve(){
-    int range = g_size;
-    for(int i = 0; i <g_data.size(); i++){
-        if(g_data[i] == '-'){
-            range /= 2;
-        }else{
-            int cnt = 0;
-            while(cnt < range){
-                cnt++;
-                g_result.push_back(g_data[i] - '0');
-            }
+void mySolve(int s, int r){
+    if(Check()){
+        return;
+    }
+
+    if(g_check[s]){
+        s++;
+    }
+
+    if(g_data[s] == '-'){
+        g_check[s] = true;
+        mySolve(s, r/2);
+        mySolve(s + r/2, r/2);
+    }else{
+        for(int i = 0; i < r; i++){
+            g_check[s] = true;
+            g_result.push_back(g_data[s] - '0');
         }
     }
 }
@@ -52,7 +75,7 @@ void mySolve(){
 int main()
 {
     Input();
-    mySolve();
+    mySolve(0, g_size);
     Output();
     return 0;
 }
