4793f1d90ff6cb841600a38c89aee6304a43210b
Hwangtaehun <r283@naver.com>
2025-09-04 20:02:40 +0900

    기초문제2 완성

---
+++ fbasic2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, r = 0;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        r += i;
+    }
+    cout << r << endl;
+    return 0;
+}
