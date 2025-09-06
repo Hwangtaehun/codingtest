28a40e1e3867c5406df59a7524f3f2253f609e84
Hwangtaehun <r283@naver.com>
2025-09-06 20:57:11 +0900

    F3 모범답안 확인

--- F3/main.cpp
+++ F3/main.cpp
@@ -4,23 +4,30 @@
 
 using namespace std;
 int g_dis, g_min = 0x0fffffff;
-vector<int> g_abl;
+vector<int> g_able;
+
+//solve
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
+
+int a, b, able[SIZE], ans = 2000, d[1001];
 
 void Input(){
-    int m_s, m_g;
     freopen("input.txt", "r", stdin);
-    scanf("%d %d", &m_s, &m_g);
+    scanf("%d %d", &a, &b);
     for(int i = 0; i < SIZE; i++){
-        int m_n;
-        scanf("%d", &m_n);
-        g_abl.push_back(m_n);
+        scanf("%d", &able[i]);
+        g_able.push_back(able[i]);
     }
     fclose(stdin);
 
-    g_dis = m_g - m_s;
+    g_dis = b - a;
 }
 
 void Output(){
+    if(g_min == 0x0fffffff){
+        g_min = -1;
+    }
+
     freopen("output.txt", "w", stdout);
     printf("%d", g_min);
     fclose(stdout);
@@ -28,20 +35,70 @@ void Output(){
 
 void mySolve(int sum, int cnt){
     if(sum >= g_dis){
-        if(cnt < g_min){
+        if(cnt < g_min && sum == g_dis){
             g_min = cnt;
         }
         return;
     }
 
     for(int i = 0; i < SIZE; i++){
-        mySolve(sum + g_abl[i], cnt + 1);
+        mySolve(sum + g_able[i], cnt + 1);
+    }
+}
+
+void Solve(){
+    if(a == b)
+        printf("0");
+    else{
+        for(int i = a; i <= b; i++){
+            d[i] = 123456789;
+        }
+
+        d[a] = 0;
+
+        for(int i = a + 1; i <= b; i++){
+            int temp = 123456789;
+            for(int k = 0; k < 3; k++){
+                if(i - able[k] >= a){
+                    temp = MIN(temp, d[i - able[k]] + 1);
+                }
+                d[i] = temp;
+            }
+        }
+
+        if(d[b] != 123456789){
+            printf("%d", d[b]);
+        }else{
+            printf("-1");
+        }
+    }
+}
+
+void Back(int next, int cnt){
+    if(next > b)
+        return;
+
+    if(b == next){
+        if(cnt < ans)
+            ans = cnt;
+    }
+
+    for(int i = 0; i < SIZE; i++){
+        Back(next + able[i], cnt + 1);
     }
 }
 
 int main()
 {
     Input();
+    Back(a, 0);
+    if(ans != 2000){
+        printf("%d\n", ans);
+    }
+    else{
+        printf("-1\n");
+    }
+    Solve();
     mySolve(0, 0);
     Output();
     return 0;
