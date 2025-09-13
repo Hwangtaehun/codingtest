94210771fe4de8299a04b34129bfeffad709b6ba
Hwangtaehun <r283@naver.com>
2025-09-13 21:39:13 +0900

    수정

--- F11/main.cpp
+++ F11/main.cpp
@@ -4,13 +4,16 @@
 using namespace std;
 
 int g_size, g_rest, g_com;
-vector<int> g_case;
+int *g_case;
 
 void Input(){
     freopen("input.txt", "r", stdin);
     scanf("%d %d", &g_size, &g_rest);
     fclose(stdin);
-    g_case.resize(g_size);
+    g_case = new int[g_size + 1];
+    for(int i = 0; i <= g_size; i++){
+        g_case[i] = 0;
+    }
 }
 
 void Output(){
@@ -21,14 +24,14 @@ void Output(){
 
 int mySolve(int num){
     if(num == 0){
-        //g_case[num] = 1;
-        return 1;
+        g_case[num] = 1;
+        return g_case[num];
     }else if(num == 1){
-        //g_case[num] = 1;
-        return 1;
+        g_case[num] = 1;
+        return g_case[num];
     }else{
-        //g_case[num] = 2 * mySolve(num - 2) + mySolve(num - 1);
-        return 2 * mySolve(num - 2) + mySolve(num - 1);
+        g_case[num] = 2 * mySolve(num - 2) + mySolve(num - 1);
+        return g_case[num];
     }
 }
 
