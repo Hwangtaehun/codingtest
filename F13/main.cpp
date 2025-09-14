587f48ff6a9c170d6d782d2b970795c61ff62dce
Hwangtaehun <r283@naver.com>
2025-09-14 21:46:15 +0900

    F13 모범답안 확인 완료

--- F13/main.cpp
+++ F13/main.cpp
@@ -6,9 +6,15 @@ using namespace std;
 int g_row, g_col;
 vector<vector <int> > g_data;
 
+//solve
+#include <memory.h>
+int n, m, DT[110][110];
+
 void Input(){
     freopen("input.txt", "r", stdin);
     scanf("%d %d", &g_row, &g_col);
+    n = g_row;
+    m = g_col;
     fclose(stdin);
     g_data.resize(g_row + 1);
     for(int i = 0; i <= g_row; i++){
@@ -40,7 +46,7 @@ bool Check(int r, int c){
     return false;
 }
 
-bool Avail(int r, int c){
+bool Available(int r, int c){
     if(!c || (double)g_row/g_col <= (double)r/c){
         return true;
     }
@@ -50,7 +56,7 @@ bool Avail(int r, int c){
 void mySolve(){
     for(int i = 1; i <= g_row; i++){
         for(int j = 0; j <= g_col; j++){
-            if(Avail(i, j) || j == g_col){
+            if(Available(i, j)){
                 int a = 0, b = 0;
                 if(Check(i - 1, j)){
                     a = g_data[i - 1][j];
@@ -64,9 +70,40 @@ void mySolve(){
     }
 }
 
+int f1(int a, int b){
+    if(DT[a][b] == 0){
+        if(a > n || b > m || !Available(a, b)){
+            DT[a][b] = 0;
+        }else if(a == n && b == m){
+            DT[a][b] = 1;
+        }else{
+            DT[a][b] = f1(a + 1, b) + f1(a, b + 1);
+        }
+    }
+
+    return DT[a][b];
+}
+
+int f2(int a, int b){
+    if(DT[a][b] == 0){
+        if(a == 0 && b == 0){
+            DT[a][b] = 1;
+        }else if(!Available(a, b)){
+            DT[a][b] = 0;
+        }else{
+            DT[a][b] = f2(a - 1, b) + f2(a, b - 1);
+        }
+    }
+
+    return DT[a][b];
+}
+
 int main()
 {
     Input();
+    printf("%d\n", f1(0, 0));
+    memset(DT, 0, sizeof(DT));
+    printf("%d\n", f2(n, m));
     mySolve();
     Output();
     return 0;
