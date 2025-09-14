2a2c186ac9e8126f726f4f9e9de3c703a54cc0ad
Hwangtaehun <r283@naver.com>
2025-09-14 21:27:40 +0900

    F13 완성

---
+++ F13/main.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int g_row, g_col;
+vector<vector <int> > g_data;
+
+void Input(){
+    freopen("input.txt", "r", stdin);
+    scanf("%d %d", &g_row, &g_col);
+    fclose(stdin);
+    g_data.resize(g_row + 1);
+    for(int i = 0; i <= g_row; i++){
+        g_data[i].resize(g_col + 1);
+    }
+    g_data[0][0] = 1;
+}
+
+void Output(){
+    freopen("output.txt", "w", stdout);
+    printf("%d", g_data[g_row][g_col]);
+    fclose(stdout);
+}
+
+void Print(){
+    printf("%d %d\n", g_row, g_col);
+    for(int i = 0; i <= g_row; i++){
+        for(int j = 0; j <= g_col; j++){
+            printf("%d ", g_data[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+bool Check(int r, int c){
+    if(0 <= r && r <= g_row && 0 <= c && c <= g_col){
+        return true;
+    }
+    return false;
+}
+
+bool Availble(int r, int c){
+    if((double)g_row/g_col <= (double)r/c){
+        return true;
+    }
+    return false;
+}
+
+void mySolve(){
+    for(int i = 1; i <= g_row; i++){
+        for(int j = 0; j <= g_col; j++){
+            if(Availble(i, j) || j == g_col){
+                int a = 0, b = 0;
+                if(Check(i - 1, j)){
+                    a = g_data[i - 1][j];
+                }
+                if(Check(i, j - 1)){
+                    b = g_data[i][j - 1];
+                }
+                g_data[i][j] = a + b;
+            }
+        }
+    }
+}
+
+int main()
+{
+    Input();
+    //Print();
+    mySolve();
+    Output();
+    return 0;
+}
