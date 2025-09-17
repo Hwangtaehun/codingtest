c4ebb695e244953b2f71e8c4d5851ed48e917d9d
Hwangtaehun <r283@naver.com>
2025-09-17 20:25:34 +0900

    모범답안 확인

--- F15/main.cpp
+++ F15/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-#include <fstream>
+#include <cstdio>
+#include <string.h>
 #include <vector>
 #define SIZE 2
 
@@ -9,63 +9,36 @@ int g_cnt = 0;
 vector<char> g_words;
 vector<vector <char> > g_brige;
 
+//solve
+char rol[30], dol[2][120];
+int DT1[2][120][30], DT2[2][30], rc;
+
 void Input(){
     g_brige.resize(SIZE);
 
-    ifstream inFile("input.txt");
+    freopen("input.txt", "r", stdin);
 
-    if (!inFile.is_open()) {
-        std::cerr << "파일을 열 수 없습니다." << std::endl;
-        return;
+    scanf("%s %s %s", rol, dol[0], dol[1]);
+
+    for(int i = 0; rol[i] != '\0'; i++){
+        g_words.push_back(rol[i]);
     }
 
-    string m_data;
-
-    getline(inFile, m_data);
-
-    for(int i = 0; i < m_data.size(); i++){
-        g_words.push_back(m_data[i]);
+    for(int i = 0; dol[0][i] != '\0'; i++){
+        g_brige[0].push_back(dol[0][i]);
     }
 
-    int index = 0;
-
-    while(getline(inFile, m_data)){
-
-        for(int i = 0; i < m_data.size(); i++){
-            g_brige[index].push_back(m_data[i]);
-        }
-
-        index++;
+    for(int i = 0; dol[1][i] != '\0'; i++){
+        g_brige[1].push_back(dol[1][i]);
     }
 
-    inFile.close();
+    fclose(stdin);
 }
 
 void Output(){
-    ofstream outFile("output.txt");
-
-    if (!outFile.is_open()) {
-        cerr << "파일을 열 수 없습니다." << endl;
-        return;
-    }
-
-    outFile << g_cnt;
-
-    outFile.close();
-}
-
-void Print(){
-    for(int i = 0; i < g_words.size(); i++){
-        cout << g_words[i];
-    }
-    cout << endl;
-
-    for(int i = 0; i < g_brige.size(); i++){
-        for(int j = 0; j < g_brige[i].size(); j++){
-            cout << g_brige[i][j];
-        }
-        cout << endl;
-    }
+    freopen("output.txt", "w", stdout);
+    printf("%d", g_cnt);
+    fclose(stdout);
 }
 
 //-1: 0, 1: 1
@@ -91,13 +64,49 @@ void mySolve(int b_num, int w_num, int sw){
     mySolve(b_num + 1, w_num, sw);
 }
 
+int f1(int dolnum, int dolpos, int rolpos){
+    if(DT1[dolnum][dolpos][rolpos] == 0){
+        if(rol[rolpos] == '\0'){
+            DT1[dolnum][dolpos][rolpos] = 1;
+        }
+
+        for(int i = dolpos; dol[1 - dolnum][i] != '\0'; i++){
+            if(dol[1 - dolnum][i] == rol[rolpos]){
+                DT1[dolnum][dolpos][rolpos] += f1(1 - dolnum, i + 1, rolpos + 1);
+            }
+        }
+    }
+
+    return DT1[dolnum][dolpos][rolpos];
+}
+
+int f2(int k){
+    for(int i = 0; dol[1][i]; i++){
+        for(int t = k - 1; t >= 0; t--){
+            if(dol[0][i] == rol[t])
+                DT2[1][t + 1] += DT2[0][t];
+            if(dol[1][i] == rol[t])
+                DT2[0][t + 1] += DT2[1][t];
+        }
+    }
+    return DT2[0][k] + DT2[1][k];
+}
+
 int main()
 {
     Input();
+
+    rc = strlen(rol);
+    DT2[0][0] = DT2[1][0] = 1;
+
+    printf("%d\n", f1(0, 0, 0) + f1(1, 0, 0));
+    printf("%d\n", f2(rc));
+
     for(int i = 0; i < g_brige[0].size(); i++){
         mySolve(i, 0, -1);
         mySolve(i, 0, 1);
     }
+
     Output();
     return 0;
 }
