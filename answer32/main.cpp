b312d47b4da37e1bfe939387ac1ad70a35d71367
Hwangtaehun <r283@naver.com>
2025-10-18 20:54:26 +0900

    문제 32번 완성

--- answer32/main.cpp
+++ answer32/main.cpp
@@ -69,8 +69,105 @@ void Output(){
     fclose(stdout);
 }
 
+struct Node {
+    int id, x, y;
+    Node *left = nullptr;
+    Node *right = nullptr;
+
+    Node(int id, int x, int y){
+        this->id = id;
+        this->x = x;
+        this->y = y;
+    }
+};
+
+class BinaryTree {
+private:
+    Node *root = nullptr;
+
+    static bool compareNodes(Node *a, Node *b) {
+        if(a->y != b->y){
+            return a->y > b->y;
+        }
+
+        return a->x < b->x;
+    }
+
+    Node *addNode(Node *current, Node *newNode) {
+        if(current == nullptr)
+            return newNode;
+
+        if(newNode->x  < current->x) {
+            current->left = addNode(current->left, newNode);
+        } else {
+            current->right = addNode(current->right, newNode);
+        }
+
+        return current;
+    }
+
+    void preOrder(Node *node, vector<int> &traversal) {
+        if(node == nullptr)
+            return;
+        traversal.push_back(node->id);
+        preOrder(node->left, traversal);
+        preOrder(node->right, traversal);
+    }
+
+    void postOrder(Node *node, vector<int> &traversal) {
+        if(node == nullptr)
+            return;
+
+        postOrder(node->left, traversal);
+        postOrder(node->right, traversal);
+        traversal.push_back(node->id);
+    }
+
+public:
+    BinaryTree() {
+        root = nullptr;
+    }
+
+    void buildTree(const vector< vector<int> > &nodeinfo){
+        vector<Node *> nodes;
+
+        for(int i = 0; i< nodeinfo.size(); ++i) {
+            nodes.push_back( new Node( i + 1, nodeinfo[i][0], nodeinfo[i][1] ) );
+        }
+
+        sort(nodes.begin(), nodes.end(), compareNodes);
+
+        for(Node *node : nodes) {
+            root = addNode(root, node);
+        }
+    }
+
+    vector<int> getPreOrderTraversal() {
+        vector<int> traversal;
+        preOrder(root, traversal);
+
+        return traversal;
+    }
+
+    vector<int> getPostOrderTraversal() {
+        vector<int> traversal;
+        postOrder(root, traversal);
+
+        return traversal;
+    }
+};
+
+void Solve(){
+    BinaryTree tree;
+    tree.buildTree(nodeinfo);
+    answer.push_back(tree.getPreOrderTraversal());
+    answer.push_back(tree.getPostOrderTraversal());
+}
+
 int main()
 {
     Input();
+    Solve();
+    Output();
     return 0;
 }
