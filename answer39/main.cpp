9ffc237bc935bb9ab2716a0978eaf7826fdc7492
Hwangtaehun <r283@naver.com>
2025-10-27 21:34:21 +0900

    업로드

--- answer39/main.cpp
+++ answer39/main.cpp
@@ -19,7 +19,7 @@ void Input(){
 
     freopen("input.txt", "r", stdin);
 
-    cin >> num_vertices >> source;
+    cin >> source >> num_vertices;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     getline(cin, s);
 
@@ -28,11 +28,9 @@ void Input(){
     s.erase(remove(s.begin(), s.end(), '['), s.end());
     s.erase(remove(s.begin(), s.end(), ']'), s.end());
 
-    cout << s << endl;
-
     stringstream ss(s);
 
-    while(ss, s, ','){
+    while(getline(ss, s, ',')){
         s.erase(0, s.find_first_not_of(" \t\n\r"));
         s.erase(s.find_last_not_of(" \t\n\r") + 1);
 
@@ -61,13 +59,65 @@ void Output(){
     fclose(stdout);
 }
 
+bool Check(vector<bool> v){
+    for(int i = 0; i < v.size(); i++){
+        if(!v[i]){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+template <typename T>
+void vector_Print(T v){
+    for(auto a: v){
+        cout << a << " ";
+    }
+    cout << endl;
+}
+
+void Solve(){
+    int s = source;
+    vector<bool> visited(num_vertices);
+
+    answer.assign(num_vertices, 0xFFFFFFF);
+    answer[s] = 0;
+    visited[s] = true;
+
+    while(Check(visited)){
+        vector<int> transfer;
+
+        for(int i = 0; i < edges.size(); i++){
+            if(get<0>(edges[i]) == s){
+                int goal = get<1>(edges[i]);
+                int value = get<2>(edges[i]);
+                answer[goal] = value;
+                transfer.push_back(goal);
+            }
+        }
+
+        for(int h = 0; h < transfer.size(); h++){
+            for(int i = 0; i < edges.size(); i++){
+                if(transfer[h] == get<0>(edges[i])){
+                    int goal = get<1>(edges[i]);
+                    int value = get<2>(edges[i]) + answer[transfer[h]];
+                    if(value < answer[goal]){
+                        answer[goal] = value;
+                    }
+                    cout << "start = " << transfer[h] << ", goal = " << goal << endl;
+                }
+            }
+        }
+    }
+
+    vector_Print(answer);
+}
+
 int main()
 {
     Input();
-    for (auto &e : edges) {
-        cout << get<0>(e) << ", "
-             << get<1>(e) << ", "
-             << get<2>(e) << endl;
-    }
+    Solve();
+    //Output();
     return 0;
 }
